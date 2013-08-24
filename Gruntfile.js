d8c006ebc5454d09468a2f9b5a9be63a05f9c059
Rafael Lyra <rafael@rafaellyra.com>
2013-08-24 10:50:05 -0300

    Install grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function (grunt) {
+    'use strict';
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		connect: {
+			server: {
+				options: {
+					port: 9001,
+					keepalive : true
+				}
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.registerTask('server', ['connect:server']);
+}
