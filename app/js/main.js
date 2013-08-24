c6f755f2bae7c81ce4c698219b7a9f218dd016ac
Hugo Campos <hugofcampos@gmail.com>
2013-08-24 16:47:18 -0300

    Distance calculating

--- app/js/main.js
+++ app/js/main.js
@@ -14,9 +14,9 @@ Instacloser.prototype.init = function () {
 	this.getAccessToken();
 	this.getLocalization();
 
-	if(this.isLoggedIn()){
-		this.asyncPics();
-	}
+	// if(this.isLoggedIn()){
+	// 	this.asyncPics();
+	// }
 }
 Instacloser.prototype.getAccessToken = function() {
 	var hash = getHash();
@@ -35,16 +35,19 @@ Instacloser.prototype.isLoggedIn = function() {
 	return '' != this.accessToken;
 }
 Instacloser.prototype.getLocalization = function() {
-	this.lat = -30.060472;
-	this.lng = -51.175533;
+	// this.lat = -30.060472;
+	// this.lng = -51.175533;
+
+	geolocation($.proxy(this.asyncPics, this));
 }
-Instacloser.prototype.asyncPics = function() {
+
+Instacloser.prototype.asyncPics = function(coordenates) {
 	var self = this;
-	lat = this.lat;
-	lng = this.lng;
+	this.lat = coordenates.lat;
+	this.lng = coordenates.lng;
 	accessToken = this.accessToken;
 	$.ajax({
-	    url : 'https://api.instagram.com/v1/media/search?lat='+lat+'&lng='+lng+'&access_token='+accessToken+'',
+	    url : 'https://api.instagram.com/v1/media/search?lat='+this.lat+'&lng='+this.lng+'&access_token='+accessToken+'',
 	    dataType : 'jsonp',
 	    success : $.proxy(self.drawPics, self),
 	    error : function (error) {
@@ -54,15 +57,49 @@ Instacloser.prototype.asyncPics = function() {
 };
 Instacloser.prototype.drawPics = function(res) {
 	var img, item;
+	var self = this;
 	$.each(res.data, function (i, picture) {
 		item = $('<li>').addClass('picture-list-item');
 		img = $('<img>').attr('src', picture.images.thumbnail.url);
+
+		// calculating distance
+		d = self.calcDistance(picture.location.latitude, picture.location.longitude, 'K');
+		if(d<1){
+			dtext = Math.round( d*1000 ) + 'm';
+		}
+		else{
+			dtext = d.toFixed(2) + 'Km';
+		}
+
+		distance = $('<p>' + dtext + '</p>');
+		
 		img.appendTo(item);
+		distance.appendTo(item);
+
 		$('.pictures-list ul').append(item);
-		console.log(picture.images);
+		// console.log(picture);
 	});
 	this.flipBox.toggle();
 };
+Instacloser.prototype.calcDistance = function(lat, lng, unit) {
+
+	var radlat1 = Math.PI * this.lat/180
+	var radlat2 = Math.PI * lat/180
+	var radlon1 = Math.PI * this.lng/180
+	var radlon2 = Math.PI * lng/180
+
+	// theta = (lgn where we are - lng where the picture is)
+	var theta = this.lng-lng;
+	var radtheta = Math.PI * theta/180
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist)
+	dist = dist * 180/Math.PI
+	dist = dist * 60 * 1.1515
+	if (unit=="K") { dist = dist * 1.609344 }
+	if (unit=="N") { dist = dist * 0.8684 }
+	return dist
+
+}
 
 
 function getHash() {
