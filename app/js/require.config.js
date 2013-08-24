08a2b116c3cf64c3fb6abdcd93bba54bab033884
Rafael Lyra <rafael@rafaellyra.com>
2013-08-24 14:19:24 -0300

    Brick implement.

---
+++ app/js/require.config.js
@@ -0,0 +1,12 @@
+require.config({
+    shim: {
+        brick: {
+            exports: 'brick'
+        }
+    },
+    paths: {
+        brick: '../vendor/components/brick/dist/brick',
+        zepto: '../vendor/components/zepto/zepto',
+        components: '/components'
+    }
+});
