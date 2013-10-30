0a818eb733de6a3f28f917e7c9a638e47862a2eb
Felquis Gimenes <frgformenton@gmail.com>
2013-10-30 04:11:24 -0200

    Add JSLint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,40 @@
 module.exports = function (grunt) {
     'use strict';
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		connect: {
-			server: {
-				options: {
-					port: 8080,
-					keepalive : true,
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    keepalive : true,
                     base: 'app/',
                     hostname: '*'
-				}
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.registerTask('server', ['connect:server']);
-}
+                }
+            }
+        },
+        jslint: {
+            node: {
+                src: ['Gruntfile.js'],
+                directives: {
+                    node: true,
+                    indent: 4
+                }
+            },
+            client: {
+                src: ['app/js/{,*}/*.js'],
+                directives: {
+                    browser: true,
+                    indent: 4,
+                    devel: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-jslint');
+
+    grunt.registerTask('server', ['jslint', 'connect:server']);
+    grunt.registerTask('build', ['jslint']);
+};
--- app/js/geolocationPosition.js
+++ app/js/geolocationPosition.js
@@ -1,30 +1,22 @@
 function geolocation(f) {
-    "use strict";
+    'use strict';
 
     if (!navigator.geolocation) {
-        /*
-            @TODO
-            Mudar o alert para mensgens de erro para o usuário
-        */
-        alert("Geolocation is not supported by your browser");
+        alert('Geolocation is not supported by your browser');
         return;
     }
 
     function success(position) {
         var geo = {
-            "lat":position.coords.latitude, 
-            "lng":position.coords.longitude
+            'lat': position.coords.latitude,
+            'lng': position.coords.longitude
         };
-        
+
         f(geo);
     }
 
     function error() {
-        /*
-            @TODO
-            Mudar o alert para mensgens de erro para o usuário
-        */
-        alert("Unable to retrieve your location");
+        alert('Unable to retrieve your location');
     }
 
     navigator.geolocation.getCurrentPosition(success, error);
