a6ab3a8611dd87f4b2e033eb695ff2443bfa1c20
Ofelquis Gimenes <ofelquis.gimenes@tribointeractive.com.br>
2014-01-20 17:08:36 -0200

    Fix - copy cwd path

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function (grunt) {
             },
             build: {
                 expand: true,
-                cwd: '.temp/',
+                cwd: 'app/',
                 src: '**',
                 dest: 'build/'
             }
