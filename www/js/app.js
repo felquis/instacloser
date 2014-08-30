8a7bdf761366072d59eda0e54264a36dde91788b
Felquis Gimenes <frgformenton@gmail.com>
2014-08-30 14:55:07 -0300

    Add editorconfig :facepunch:

--- www/js/app.js
+++ www/js/app.js
@@ -69,4 +69,3 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
 });
-
