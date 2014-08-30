7c8ef4e12a9b247b1ea52a8e12f05be8f44ad076
Felquis Gimenes <frgformenton@gmail.com>
2014-08-30 19:35:18 -0300

    Add login views

--- www/js/app.js
+++ www/js/app.js
@@ -24,48 +24,32 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   $stateProvider
 
     .state('app', {
-      url: "/app",
+      url: '/app',
       abstract: true,
-      templateUrl: "templates/menu.html",
+      templateUrl: 'templates/menu.html',
       controller: 'AppCtrl'
     })
 
-    .state('app.search', {
-      url: "/search",
+    .state('app.login', {
+      url: '/login',
       views: {
         'menuContent' :{
-          templateUrl: "templates/search.html"
+          templateUrl: 'templates/login.html',
+          controller: 'LoginCtrl'
         }
       }
     })
 
-    .state('app.browse', {
-      url: "/browse",
+    .state('app.nearby', {
+      url: '/nearby',
       views: {
         'menuContent' :{
-          templateUrl: "templates/browse.html"
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-    .state('app.single', {
-      url: "/playlists/:playlistId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlist.html",
-          controller: 'PlaylistCtrl'
+          templateUrl: 'templates/nearby.html',
+          controller: 'NearbyCtrl'
         }
       }
     });
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/login');
 });
