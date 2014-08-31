8b140c65ef13367f57ef2757bf0f91835244159e
Felquis Gimenes <frgformenton@gmail.com>
2014-08-30 23:01:46 -0300

    Loading basic timeline

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -31,8 +31,33 @@ angular.module('starter.controllers', [])
   }
 })
 
-.controller('NearbyCtrl', function () {
-  console.log('NearbyCtrl');
+.controller('NearbyCtrl', function ($scope, $http, $state) {
 
+  if (!localStorage['ic-instagram-token']) {
+    $state.go('app.login');
 
+    return
+  }
+
+  $scope.loadedItems = [];
+
+  $scope.loadMore = function (nextPage) {
+    $http({
+      method: 'jsonp',
+      url: ['https://api.instagram.com/v1/media/search?lat=48.858844',
+        'lng=2.294351',
+        'distance=5000',
+        'access_token=' + localStorage['ic-instagram-token'],
+        'callback=JSON_CALLBACK'].join('&')
+    })
+    .success(function(success) {
+      console.log(success);
+      $scope.loadedItems = success.data;
+    })
+    .error(function (err) {
+      console.log(err);
+    })
+  }
+
+  $scope.loadMore();
 });
