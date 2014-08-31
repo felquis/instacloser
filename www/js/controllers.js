3d82b2277e4c72fcab2fb61b54b42d81d8e1340d
Felquis Gimenes <frgformenton@gmail.com>
2014-08-30 21:50:54 -0300

    Add functionality to delete localStorage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,24 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $state) {
+  if (!!localStorage['ic-instagram-token']) {
+    $scope.logged = true;
+    $scope.loginPhrase = 'Logout'
+  } else {
+    $scope.logged = false;
+    $scope.loginPhrase = 'Login'
+  }
 
+  $scope.loginOrDie = function(event) {
+    if (!!localStorage['ic-instagram-token']) {
+      localStorage.removeItem('ic-instagram-token');
+      $state.go('app.login');
+      $scope.logged = true;
+      $scope.loginPhrase = 'Login'
+    } else {
+      $state.go('app.login');
+    }
+  }
 })
 
 .controller('LoginCtrl', function ($scope, $state) {
