0bebd7f4312230224c217bed88bc03844fec6f70
Felquis Gimenes <frgformenton@gmail.com>
2014-09-02 13:36:31 -0300

    Add instagram authentication using inAppBrowser

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,33 +1,106 @@
 angular.module('starter.controllers', ['ngCordova'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $state) {
-  if (!!localStorage['ic-instagram-token']) {
-    $scope.logged = true;
-    $scope.loginPhrase = 'Logout'
-  } else {
-    $scope.logged = false;
-    $scope.loginPhrase = 'Login'
-  }
+
+  $scope.checkAccessToken = function () {
+    if (!!localStorage['ic-instagram-token']) {
+      $scope.logged = true;
+      $scope.loginPhrase = 'Logout'
+    } else {
+      $scope.logged = false;
+      $scope.loginPhrase = 'Login'
+    }
+  };
+
+  $scope.checkAccessToken();
 
   $scope.loginOrDie = function(event) {
     if (!!localStorage['ic-instagram-token']) {
       localStorage.removeItem('ic-instagram-token');
       $state.go('app.login');
       $scope.logged = true;
-      $scope.loginPhrase = 'Login'
+      $scope.loginPhrase = 'Login';
     } else {
       $state.go('app.login');
     }
   }
+
+  $scope.baseURL = 'http://192.168.1.107:8080';
+  $scope.redirect_uri = encodeURIComponent($scope.baseURL + '/login.html');
+
+  if ($scope.baseURL === 'http://localhost:8080') {
+    $scope.client_id = 'a2dadbd1d44f4e4a869d3fd3ab8543fc';
+  } else if ($scope.baseURL === 'http://instacloser.com') {
+    $scope.client_id = '05d5219366e24a3bb9f4d7eec6427e52';
+  } else if ('http://192.168.1.107:8080') {
+    $scope.client_id = 'f4c8a95c47ef41e4a050159f41dad857';
+  }
 })
 
 .controller('LoginCtrl', function ($scope, $state) {
-  console.log('LoginCtrl');
+  /*
+    TODO:
+      To logout a user try open an InAppBrowser to > https://instagram.com/accounts/logout/
+      and delete localStorage['ic-instagram-token']
+
+      Next time, the user will need to login again
+  */
 
   $scope.loading = false;
 
-  $scope.onClickLogin = function () {
+  window.popup = '';
+
+  $scope.onClickLogin = function (event) {
+
     $scope.loading = true;
+
+    var width = 380,
+        height = 400,
+        left = (window.outerWidth / 2) - (width / 2),
+        top = (window.outerHeight / 2) - (height / 2);
+
+    popup = window.open('https://instagram.com/oauth/authorize/?client_id=' + $scope.client_id + '&amp;redirect_uri=' + $scope.redirect_uri + '&amp;response_type=token', '_blank', 'location=yes,resizable=1,width='+ width +',height=' + height + ',top=' + top + ',left=' + left);
+
+    // On unLoad save the access_token in localStorage
+    popup.onunload = function (event) {
+      if (popup.location.hash.indexOf('#access_token=') !== -1) {
+        if (!!localStorage['ic-instagram-token']) {
+          // If exist, do not do nothing.
+        } else {
+          localStorage['ic-instagram-token'] = popup.location.hash.replace(/^#[\w\W]*(access_token=([\.\d\w]+))[\w\W]*$/i, '$2');
+        }
+
+        $scope.checkAccessToken();
+
+        $state.go('app.nearby');
+      }
+    }
+
+    // Popup will ask to be closed
+    // It works on desktop and mobile browsers (not an app)
+    window.addEventListener('message', function(event) {
+      if (event.origin === $scope.baseURL) {
+        popup.close();
+
+        window.removeEventListener('message');
+      }
+    });
+
+    // It works for Android/iOS built app
+    popup.addEventListener('loadstart', function (event) {
+      if (event.url.indexOf($scope.baseURL) > -1) {
+
+        var url = event.url.split('login.html')
+
+        localStorage['ic-instagram-token'] = url[1].replace(/^#[\w\W]*(access_token=([\.\d\w]+))[\w\W]*$/i, '$2');
+
+        $scope.checkAccessToken();
+
+        popup.close();
+
+        $state.go('app.nearby');
+      }
+    });
   }
 })
 
